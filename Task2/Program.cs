b752911a8ead0ac4d2c7124e03234fb6819946e0
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-02 16:12:16 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Для сравнения двух чисел введите первое число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+if (numberFirst < numberSecond)
+{
+    Console.WriteLine("Больше число второе число: " + numberSecond);
+}
+else
+{
+    Console.WriteLine("Больше число первое число: " + numberFirst);
+}
