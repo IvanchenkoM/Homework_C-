adaf508ce45ef80384382f6156e4a8500e6ecd59
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-02 16:48:44 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число, чтобы проверить является ли оно четным: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = number % 2;
+if (evenNumber == 0)
+{
+    Console.Write("Ваше число " + number + " является четным.");
+}
+else
+{
+    Console.Write("Ваше число " + number + " является нечетным.");
+}
