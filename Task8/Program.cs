9808857534ad40a04716eb741e5cc747539d1f15
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-02 17:11:15 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Чтобы узнать все четные числа от 1 до числа, введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+if (number < 1) 
+{
+    Console.Write("Вы введи некоректрное число, попробуйте снова!");
+}
+else
+{
+    Console.Write("От 1 до вашего числа " + number + " находятся следующие четные числа: ");
+}
+while (evenNumber < number)
+{
+    Console.Write(" " + evenNumber + " ");
+    evenNumber +=2;
+}
